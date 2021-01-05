d73268b23953b8dc684e7c5a38b4d81cf964002b
McMarrell-Pro <sanketyadav984@gmail.com>
2021-01-05 18:40:42 +0530

    Added SlingShot

---
+++ slingshot.js
@@ -0,0 +1,15 @@
+class SlingShot{
+    constructor(bodyA,bodyB){
+    var options = {bodyA:bodyA,
+    bodyB:bodyB,
+    stiffness:0.04,
+    lenght:20}
+    this.sling = Constraint.create(options);
+    World.add(world,this.sling);
+    }
+    display(){
+    var pointA = this.sling.bodyA.position;
+    var pointB = this.sling.bodyB.position;
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+}
